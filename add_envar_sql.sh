d3fb1fc3882122fd7736bfc41f89512f5162fbc1
MihaiBorsu <mihai.borsu@gmail.com>
2020-11-19 17:02:52 +0200

    added API better

---
+++ add_envar_sql.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export MSSQL_SA_PASSWORD='@FreeYourself'
+export MSSQL_PID='evaluation'
+export SQL_INSTALL_AGENT='y'
+export SQL_INSTALL_USER='@FreeYourself'
+export SQL_INSTALL_USER_PASSWORD='@FreeYourself'
+export SQL_INSTALL_AGENT='y'
