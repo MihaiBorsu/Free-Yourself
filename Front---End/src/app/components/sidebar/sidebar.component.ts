1ba213df4d0f1551b89d01eb3382d11b3388e3a0
Walter Sipos <walter.sipos@avaelgo.ro>
2020-12-14 14:53:31 +0200

    Dockerfile added

--- Front---End/src/app/components/sidebar/sidebar.component.ts
+++ Front---End/src/app/components/sidebar/sidebar.component.ts
@@ -10,13 +10,13 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
-    { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
+    { path: '/table-list', title: 'Rankings',  icon:'content_paste', class: '' },
     { path: '/stolen-vehicles', title: 'Stolen Vehicles',  icon:'highlight_off', class: '' },
     { path: '/guild', title: 'Guild',  icon:'groups', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
+    // { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
+    // { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
     { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
+    // { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
     { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
 ];
 
