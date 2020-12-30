6b7c399fcc254a5934904410350035c51b2c3b50
Walter Sipos <walter.sipos@avaelgo.ro>
2020-12-30 21:27:44 +0200

    dashboard solved

--- Front---End/src/app/dashboard/dashboard.component.ts
+++ Front---End/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as Chartist from 'chartist';
 import { showNotification } from '../helpers/notification'
 import { XpService } from "../services/xp.service";
+import { UserService } from '../services/user.service'
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +11,20 @@ import { XpService } from "../services/xp.service";
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private xpService: XpService) { }
+  // dashboard:{
+  //   userId: number,
+  //   dailyXp: number,
+  //   weeklyXp: number,
+  //   monthlyXp: number,
+  //   yearlyXp: number,
+  //   sevenDaysXp: number[],
+  //   twelveMonthsXp: number[]
+  // }
+  dashboard: any
+
+  constructor(private xpService: XpService,
+             private userService : UserService) { }
+
   startAnimationForLineChart(chart){
       let seq: any, delays: any, durations: any;
       seq = 0;
@@ -68,85 +82,106 @@ export class DashboardComponent implements OnInit {
       seq2 = 0;
   };
   ngOnInit() {
-      /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
-      showNotification('top','center', 'Move you body!')
-      const dataDailySalesChart: any = {
-          labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-          series: [
-              [12, 17, 7, 17, 23, 18, 38]
-          ]
-      };
+    let user = JSON.parse(localStorage.getItem('user'));
+    this.userService.getDashboard(user.id).subscribe(
+      res => {
+        this.dashboard = res
+        console.log(res)
+        console.log("Working getGuilds")
+        // showNotification('top','center', 'Between Titans')
+      
+       /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
+       showNotification('top','center', 'Move you body!')
+       
+       this.dashboard.sevenDaysXp = [20,32,21,38,6,24,3]
+ 
+       const dataDailySalesChart: any = {
+         labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+         series: [this.dashboard.sevenDaysXp.reverse()]
+     };
 
-     const optionsDailySalesChart: any = {
-          lineSmooth: Chartist.Interpolation.cardinal({
-              tension: 0
-          }),
-          low: 0,
-          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
-      }
+     console.log(dataDailySalesChart.series)
+ 
+      const optionsDailySalesChart: any = {
+           lineSmooth: Chartist.Interpolation.cardinal({
+               tension: 0
+           }),
+           low: 0,
+           high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+           chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
+       }
+ 
+       var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+ 
+       this.startAnimationForLineChart(dailySalesChart);
+ 
+ 
+       /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
+ 
+       const dataCompletedTasksChart: any = {
+           labels: ['12p', '3p', '6p', '9p', '12p', '3a', '6a', '9a'],
+           series: [
+               [230, 750, 450, 300, 280, 240, 200, 190]
+           ]
+       };
+ 
+      const optionsCompletedTasksChart: any = {
+           lineSmooth: Chartist.Interpolation.cardinal({
+               tension: 0
+           }),
+           low: 0,
+           high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+           chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
+       }
+ 
+       var completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
+ 
+       // start animation for the Completed Tasks Chart - Line Chart
+       this.startAnimationForLineChart(completedTasksChart);
+ 
+ 
+ 
+       /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
+ 
+       this.dashboard.twelveMonthsXp = [200,320,210,380,60,240,30,200,320,210,380,60,240,30]
 
-      var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
-
-      this.startAnimationForLineChart(dailySalesChart);
-
-
-      /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
-
-      const dataCompletedTasksChart: any = {
-          labels: ['12p', '3p', '6p', '9p', '12p', '3a', '6a', '9a'],
-          series: [
-              [230, 750, 450, 300, 280, 240, 200, 190]
-          ]
-      };
-
-     const optionsCompletedTasksChart: any = {
-          lineSmooth: Chartist.Interpolation.cardinal({
-              tension: 0
-          }),
-          low: 0,
-          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
-      }
-
-      var completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
-
-      // start animation for the Completed Tasks Chart - Line Chart
-      this.startAnimationForLineChart(completedTasksChart);
-
-
-
-      /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
-
-      var datawebsiteViewsChart = {
-        labels: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
-        series: [
-          [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
-
-        ]
-      };
-      var optionswebsiteViewsChart = {
-          axisX: {
-              showGrid: false
-          },
-          low: 0,
-          high: 1000,
-          chartPadding: { top: 0, right: 5, bottom: 0, left: 0}
-      };
-      var responsiveOptions: any[] = [
-        ['screen and (max-width: 640px)', {
-          seriesBarDistance: 5,
-          axisX: {
-            labelInterpolationFnc: function (value) {
-              return value[0];
-            }
-          }
-        }]
-      ];
-      var websiteViewsChart = new Chartist.Bar('#websiteViewsChart', datawebsiteViewsChart, optionswebsiteViewsChart, responsiveOptions);
-
-      //start animation for the Emails Subscription Chart
-      this.startAnimationForBarChart(websiteViewsChart);
+       var datawebsiteViewsChart = {
+         labels: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
+         series: [
+          this.dashboard.twelveMonthsXp.reverse()
+ 
+         ]
+       };
+       var optionswebsiteViewsChart = {
+           axisX: {
+               showGrid: false
+           },
+           low: 0,
+           high: 400,
+           chartPadding: { top: 0, right: 5, bottom: 0, left: 0}
+       };
+       var responsiveOptions: any[] = [
+         ['screen and (max-width: 640px)', {
+           seriesBarDistance: 5,
+           axisX: {
+             labelInterpolationFnc: function (value) {
+               return value[0];
+             }
+           }
+         }]
+       ];
+       var websiteViewsChart = new Chartist.Bar('#websiteViewsChart', datawebsiteViewsChart, optionswebsiteViewsChart, responsiveOptions);
+ 
+       //start animation for the Emails Subscription Chart
+       this.startAnimationForBarChart(websiteViewsChart);
+      
+      
+      
+      }, err => {
+        console.log("Error at getGuilds")
+      })
+    
+     
   }
 
   workout(activity){
